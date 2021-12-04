d98d7f440c3117787e2806f602a7c0deb0216adf
Monta Dravniece <montadravniece@gmail.com>
2021-12-04 15:28:54 +0200

    Pievienojam split difficuly no vecā koda

--- hangman.py
+++ hangman.py
@@ -1,13 +1,35 @@
-import random
 from gameplay.game import Game
+from scripts.split_difficulty import Dificulty
 
-# Tika ielasīts vārda saraksts
-with open('data/words.txt', 'r', encoding='utf-8') as file: 
-    file_content = file.read()
-    words = file_content.split("\n")
-    random.shuffle(words)
-    
+if_new_words = Dificulty(words_all=1)
+if_new_words.diff()
+
+difficulty_info = 'Hello!', 'There are 3 difficulty levels:"\n"1st - bez garumzīmēm un mīkstinājumiem and more than 6 letters"\n"2nd - ar garumzīmēm un mīkstinājumiem and more than 6 letters"\n"3rd - ar garumzīmēm un mīkstinājumiem and less than 6 letters'
+## ievadi varbūt vajag while ciklā -> atkārtoti prasa ievadi, kamēr izvēlas 1 no trim
+print(difficulty_info)
+
+while True:
+    difficulty = input("Please, chose your game difficulty level (easy/medium/hard) : ")
+
+    if difficulty == 'easy':
+        with open('data/easy_words.txt', 'r', encoding='utf-8') as file:
+            file_contents = file.read()
+            break
+    elif difficulty == 'medium':
+        with open('data/medium_words.txt', 'r', encoding='utf-8') as file:
+            file_contents = file.read()
+            break
+    elif difficulty == 'hard':
+        with open('data/hard_words.txt', 'r', encoding='utf-8') as file:
+            file_contents = file.read()
+            break
+    else:
+        print("Invalid input!")
+
+words = list(file_contents.split("', '"))
+
 # Spēles palaišana 
+
 while True:
     word = words.pop()
     game = Game(word)
--- scripts/split_difficulty.py
+++ scripts/split_difficulty.py
@@ -1,3 +1,68 @@
-class Difficulty:
-    def __init__(self, words):
-        self.words = words
+class Dificulty:
+  def __init__ (self, words_all):
+    self.words_all = words_all
+
+  def diff (self):
+    with open('data/words.txt', 'r', encoding='utf-8') as file: 
+      words_all = file.read()
+
+    words_split = []
+    words_split = words_all.split("\n")
+
+    easy_words = []
+    medium_words = []
+    hard_words = []
+
+    hard_letters = ['Ā', 'Č', 'Ē', 'Ģ', 'Ī', 'Ķ', 'Ļ', 'Ņ', 'Š', 'Ū', 'Ž']
+
+    ######## izveido vārdnīcu -> katram vārdam unikālo simbolu skaits
+    # unique_count_word = {}
+    # for word in words_split:
+    #   unique_count_word[word] = len(set(word))
+
+    # print(unique_count_word)
+    ########
+
+    #### dificulty 3
+    for words in words_split:
+      for character in hard_letters:
+        if character in words and len(words) < 7:
+          hard_words.append(words)
+
+    ### dificulty 2
+    for words in words_split:
+      for character in hard_letters:
+        if character in words and len(words) > 6:
+          medium_words.append(words)
+
+    #### difficulty 1
+    
+    a = set(words_split)
+    b = set(hard_words)
+    c = set(medium_words)
+    easy_words = a - b - c
+
+
+
+
+    #### cikls, kas vienkārši sadala visus vārdus 3 sarežģītības pakāpēs tikai pēc vārda garuma
+    # for word in words_split:
+    #   if len(word) > 6:
+    #     easy_words.append(word)
+    #   elif len(word) > 14:
+    #     hard_words.append(word)
+    #   else:
+    #     medium_words.append(word)
+    # ####
+
+    # print(easy_words)
+
+
+    with open('data/easy_words.txt', 'w', encoding='utf-8') as file: 
+      file.write(str(set(easy_words)))
+
+    with open('data/medium_words.txt', 'w', encoding='utf-8') as file:
+      file.write(str(set(medium_words)))
+
+    with open('data/hard_words.txt', 'w', encoding='utf-8') as file:
+      file.write(str(set(hard_words)))
